ba1e8df2667f0d3793f7d784045026964a966ba8
vladwarrior <vladislav.mineev.07@mail.ru>
2022-03-28 10:36:47 +0300

    третий коммит - третий этап

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function welcome()
+{
+    print_r("Welcome to the Brain Game!\n");
+    $name = prompt("May I have your name? ");
+    print_r("Hello, " . $name . "!\n");
+}
